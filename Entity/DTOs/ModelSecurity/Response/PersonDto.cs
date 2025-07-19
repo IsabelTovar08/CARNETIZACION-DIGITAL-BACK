24ebb40ac621b1291ce4f55b0dcf92a4519267d6
markiniorojas <markiniorojas@gmail.com>
2025-07-18 23:45:41 -0500

    generar dtos

--- Entity/DTOs/ModelSecurity/Response/PersonDto.cs
+++ Entity/DTOs/ModelSecurity/Response/PersonDto.cs
@@ -12,16 +12,21 @@ namespace Entity.DTOs
         public string Identification { get; set; } 
         public string? Phone { get; set; }
         public string Email { get; set; }
+        public string Address { get; set; }
+
         public int CityId { get; set; }
         public string CityName { get; set; }
+
         public int OrganizationId { get; set; }
+        public string OrganizationName { get; set; }
         public int BranchId { get; set; }
         public string BranchName { get; set; }
-        public string OrganizationName { get; set; }
+
         public int OrganizationalUnitId { get; set; }
         public string OrganizationalUnitName { get; set; }
         public int InternalDivisionId { get; set; }
         public string InternalDivisionName { get; set; }
+
         public string Photo { get; set; }
         public int ProfileId { get; set; }
         public string ProfileName { get; set; }
